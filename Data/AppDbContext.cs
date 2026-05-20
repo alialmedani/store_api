ce140d2f539c5f24700592b224d7c207fab45a6e
alialmedani <alimedani02@gmail.com>
2026-05-20 09:46:52 +0300

    Initial backend API

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Models;
+
+namespace Store.Data;
+
+public class AppDbContext : DbContext
+{
+	public AppDbContext(DbContextOptions<AppDbContext> options)
+		: base(options)
+	{
+	}
+
+	public DbSet<Category> Categories => Set<Category>();
+}
